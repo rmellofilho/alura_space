f04f899382b5f79ccc83d36f31e495ee2a20f154
Robeto Melo <rmello.filho@hotmail.com>
2023-11-29 15:42:20 -0300

    Passando uma referência

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
     fotografias = Fotografia.objects.all()
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
