5bb34ad79d5d7677a888d2cdc3b28182807b81c5
Robeto Melo <rmello.filho@hotmail.com>
2023-11-29 16:35:32 -0300

    Incluindo categoria

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,7 +3,7 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "categoria")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
 
