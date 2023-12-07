f06ad5831e47b0387aa3a53f99735089368626e3
Robeto Melo <rmello.filho@hotmail.com>
2023-12-07 14:47:16 -0300

    Funcionalidade de publicação

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,10 +3,13 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "categoria")
+    list_display = ("id", "nome", "legenda", "categoria", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
+    list_editable = ("publicada",)
     list_per_page = 10
+    
+    
 
 admin.site.register(Fotografia, ListandoFotografias)
